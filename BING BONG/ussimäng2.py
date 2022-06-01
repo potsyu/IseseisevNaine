3273f279436f5facd9fe54585fc3be0b0438105d
bjorn.johanson <bjornj955@gmail.com>
2022-06-01 16:47:36 +0300

    tere1

--- BING BONG/ussimäng2.py
+++ BING BONG/ussimäng2.py
@@ -1,48 +1,21 @@
-"""
-üssimääng
-"""
+import pygame, random, sys
 
-import random
-import sys
-import time
-import pygame
-
-pygame.init()
-pygame.mixer.init()
-
-# start timer
-start_time = time.time()  # get current time
-
-# Sound
-
-pygame.mixer.music.load('music.wav')
-pygame.mixer.music.play(-1)
-
-# Difficulty settings
-# Easy      ->  10
-# Medium    ->  25
-# Hard      ->  40
-# Harder    ->  60
-# Impossible->  120
 difficulty = 25
 
 # Window size
-frame_size_x = 691
+frame_size_x = 720
 frame_size_y = 480
 
 # Checks for errors encountered
 check_errors = pygame.init()
 # pygame.init() example output -> (6, 0)
 # second number in tuple gives number of errors
-if check_errors[1] > 0:
-    print(f'[!] Had {check_errors[1]} errors when initialising game, exiting...')
-    sys.exit(-1)
-else:
-    print('[+] Game successfully initialised')
+
 
 # Initialise game window
 pygame.display.set_caption('Snake Eater')
 game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
+
 
 # Colors (R, G, B)
 black = pygame.Color(0, 0, 0)
@@ -51,14 +24,16 @@ red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
 
+
 # FPS (frames per second) controller
 fps_controller = pygame.time.Clock()
 
+
 # Game variables
 snake_pos = [100, 50]
-snake_body = [[100, 50], [100 - 10, 50], [100 - (2 * 10), 50]]
+snake_body = [[100, 50], [100-10, 50], [100-(2*10), 50]]
 
-food_pos = [random.randrange(1, (frame_size_x // 10)) * 10, random.randrange(1, (frame_size_y // 10)) * 10]
+food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
 food_spawn = True
 
 direction = 'RIGHT'
@@ -72,32 +47,24 @@ def game_over():
     my_font = pygame.font.SysFont('times new roman', 90)
     game_over_surface = my_font.render('YOU DIED', True, red)
     game_over_rect = game_over_surface.get_rect()
-    game_over_rect.midtop = (frame_size_x / 2, frame_size_y / 4)
+    game_over_rect.midtop = (frame_size_x/2, frame_size_y/4)
     game_window.fill(black)
     game_window.blit(game_over_surface, game_over_rect)
     show_score(0, red, 'times', 20)
     pygame.display.flip()
-    pygame.mixer.quit()
-    mp3_path = 'death.mp3'
-    pygame.init()
-    pygame.mixer.init()
-    pygame.mixer.music.load(mp3_path)
-    pygame.mixer.music.play(-1)
-    time.sleep(4)
-    time.sleep(3)
     pygame.quit()
     sys.exit()
 
 
 # Score
 def show_score(choice, color, font, size):
-    score_font = pygame.font.SysFont('Minecraft.ttf', 30)
-    score_surface = score_font.render('Score : ' + str(score), True, black)
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render('Score : ' + str(score), True, color)
     score_rect = score_surface.get_rect()
     if choice == 1:
-        score_rect.midtop = (frame_size_x / 10, 15)
+        score_rect.midtop = (frame_size_x/10, 15)
     else:
-        score_rect.midtop = (frame_size_x / 2, frame_size_y / 1.25)
+        score_rect.midtop = (frame_size_x/2, frame_size_y/1.25)
     game_window.blit(score_surface, score_rect)
     # pygame.display.flip()
 
@@ -148,19 +115,16 @@ while True:
     if snake_pos[0] == food_pos[0] and snake_pos[1] == food_pos[1]:
         score += 1
         food_spawn = False
-        eat_sound = pygame.mixer.Sound("collect.wav")
-        pygame.mixer.Sound.play(eat_sound)
     else:
         snake_body.pop()
 
     # Spawning food on the screen
     if not food_spawn:
-        food_pos = [random.randrange(1, (frame_size_x // 10)) * 10, random.randrange(1, (frame_size_y // 10)) * 10]
+        food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
     food_spawn = True
 
     # GFX
-    bg = pygame.image.load("bg.jpg")  # Snake Game Background :)
-    game_window.blit(bg, [0, 0])
+    game_window.fill(black)
     for pos in snake_body:
         # Snake body
         # .draw.rect(play_surface, color, xy-coordinate)
@@ -170,12 +134,11 @@ while True:
     # Snake food
     pygame.draw.rect(game_window, white, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
 
-    game_window.blit(pygame.font.Font('Minecraft.ttf', 22).render(f"Life Wasted: {int(time.time() - start_time)}", True, [0, 0, 0]), [520, 20])  # show time elapsed
     # Game Over conditions
     # Getting out of bounds
-    if snake_pos[0] < 0 or snake_pos[0] > frame_size_x - 10:
+    if snake_pos[0] < 0 or snake_pos[0] > frame_size_x-10:
         game_over()
-    if snake_pos[1] < 0 or snake_pos[1] > frame_size_y - 10:
+    if snake_pos[1] < 0 or snake_pos[1] > frame_size_y-10:
         game_over()
     # Touching the snake body
     for block in snake_body[1:]:
